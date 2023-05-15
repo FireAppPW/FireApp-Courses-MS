7e2786bcd686e8fd5b6c9197c398c51fae2ddc44
FerGonzalez4 <fer4gonzalez@gmail.com>
2023-05-15 23:44:04 +0200

    CRUD

--- src/main/java/pw/ersms/courses/course/CourseController.java
+++ src/main/java/pw/ersms/courses/course/CourseController.java
@@ -2,16 +2,38 @@ package pw.ersms.courses.course;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("course")
 public class CourseController {
     private final CourseService courseService;
 
-    @GetMapping(path = "/course")
-    public ResponseEntity<Object> getCourse(){
-        return ResponseEntity.ok().body(courseService.getCourse());
+    @GetMapping
+    public List<Course> getCourse(){
+        return courseService.get();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Course getCourseById(@PathVariable("id") Integer id){
+        return courseService.getCourseById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createCourse(@RequestBody Course course){
+        return ResponseEntity.ok(courseService.createCourse(course));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteCourse(@PathVariable("id") Integer id){
+        courseService.deleteCourse(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Object> updateCourse(@PathVariable("id") Integer id, @RequestBody Course course){
+        return ResponseEntity.ok(courseService.updateCourse(id, course));
     }
 }
